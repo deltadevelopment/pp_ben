ab531cfae076ac1a648fd2279a27eb51cb524285
Christian Dalsvaag <christian@ddev.no>
2015-03-14 23:21:48 +0100

    Trying to fix the migration

--- db/migrate/20150314171855_change_type_of_media_type_on_messages.rb
+++ db/migrate/20150314171855_change_type_of_media_type_on_messages.rb
@@ -1,5 +1,6 @@
 class ChangeTypeOfMediaTypeOnMessages < ActiveRecord::Migration
   def change
-    change_column :messages, :media_type, :integer, :default => 0
+    remove_column :messages, :media_type 
+    add_column :messages, :media_type, :integer, :default => 0
   end
 end
